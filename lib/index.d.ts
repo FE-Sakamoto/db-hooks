2c5c508cb3559cf8ba2b87dcee92db791eaa9d64
BugMaker.Yu <BugMaker.Yu@gmail.com>
2021-02-13 08:56:42 +0800

    :new: 可以使用snapshotDB查看数据库快照

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,5 +4,6 @@ export declare function createDB<T extends Tables>(initDB: DB<T>, initId: AllID<
     useDB: <T_1 extends keyof T>(tableName: T_1, id: ID) => T[T_1];
     editDB: (edit: (db: Draft<DB<T>>) => void) => void;
     updateDB: (dbData: any) => void;
+    snapshotDB: <T_2 extends keyof T>(tableName: T_2, id: ID) => T[T_2];
 };
 export declare function merge(target: object, ...arg: object[]): object;
